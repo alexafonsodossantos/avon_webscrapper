f22de0bfa30807be86ae9983c5c0a427160f85d0
FalkonGangrel <falkon.gangrel@gmail.com>
2021-12-14 17:43:00 -0300

    Update
    Atualização dos itens para automatização para outros sites

--- avon.py
+++ avon.py
@@ -1,22 +1,14 @@
 import mysql.connector
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import time
 from bs4 import BeautifulSoup
 
 #verifique o driver do chrome antes de executar
 PATH = "./driver/cd96.exe"
 
-def times_search(t, driver):
-    contador = 0
-    while contador < t:
-        try:
-            buscaBotao = driver.find_element_by_xpath("/html/body/main/section/div[3]/div[1]/p[2]")
-            buscaBotao.click()
-            time.sleep(3)
-            contador +=1
-        except:
-            pass
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -43,7 +35,7 @@ myresultCEPS = mycursor.fetchall()
 for x in myresultCEPS:
   CEPS.append(x)
 
-sql = "SELECT * FROM tb_empresa"
+sql = "SELECT * FROM tb_empresa WHERE b_ativo = 1"
 mycursor.execute(sql)
 
 myresultEmpresas = mycursor.fetchall()
@@ -55,33 +47,40 @@ for x in myresultEmpresas:
 for empresa in EMPRESAS:
     for cep in CEPS:
 
-        driver = webdriver.Chrome(PATH)
+        ser = Service(PATH)
+        op = webdriver.ChromeOptions()
+        op.add_experimental_option('excludeSwitches', ['enable-logging'])
+        driver = webdriver.Chrome(service=ser, options=op)
+
         print("Abrindo a página...")
         driver.get(empresa[2])
 
-        time.sleep(3)
+        time.sleep(5)
 
-        buscaCep = driver.find_element_by_id("x-cep")
-        time.sleep(3)
+        buscaCep = driver.find_element(By.ID, empresa[3]) #local onde está o campo de procura
 
 
         buscaCep.send_keys(str(cep[1]))
-        time.sleep(3)
-
         buscaCep.send_keys(Keys.RETURN)
-        time.sleep(5)
+        #buscaBotao = driver.find_element(By.CLASS_NAME,'x-representante--formulario--items__lupa')
+        #buscaBotao.click()
+        time.sleep(10)
 
-
-        times_search(5, driver)
-
+        for i in range(4): #definir quantidade de vezes a ser clicado no botao de buscar começar pelo 0
+            try:
+                buscaMais = driver.find_element(By.CLASS_NAME,'js--carregarMais')
+                buscaMais.click()
+                time.sleep(10)
+            except:
+                pass
 
         html_source_code = driver.page_source
 
         soup = BeautifulSoup(html_source_code, 'html.parser')
 
-        artist_name_list = soup.find(class_='x-representante--formulario--result')
+        artist_name_list = soup.find(class_=empresa[4]) #resultado da busca
 
-        artist_name_list_items = artist_name_list.find_all('p')
+        artist_name_list_items = artist_name_list.find_all(empresa[5]) #elemento de separação dos itens
 
         dados = []
 
@@ -94,14 +93,13 @@ for empresa in EMPRESAS:
 
         for a in dados:
             for b in a:
-                if str(b) == str("<strong>Compre por: </strong>"):
+                if str(b) == str(empresa[6]): #final de registro
                     revendedores.append("0")
                 else:
                     revendedores.append(b)
 
         lista_limpa = []
         lista_menor = []
-
 
         i = 0
         while i < len(revendedores):
@@ -129,16 +127,22 @@ for empresa in EMPRESAS:
                 x[4] = e
             else:
                 continue
+
+        registros = 0
+
         for nhe in lista_limpa:
             mycursor = mydb.cursor()
 
-            sql = "INSERT INTO tb_revendedores (str_nome_revendedores, str_bairro, str_distancia, str_email, str_telefone) VALUES ( %s, %s, %s, %s, %s)"
-            val = (nhe[0], nhe[1], nhe[2], nhe[3], nhe[4])
-            mycursor.execute(sql, val)
+            mycursor.execute("SELECT str_nome_revendedores FROM tb_revendedores WHERE str_nome_revendedores = '"+nhe[0]+"'")
+            # gets the number of rows affected by the command executed
+            msg = mycursor.fetchone()  
+            # check if it is empty and print error
+            if not msg:
+                sql = "INSERT INTO tb_revendedores (str_nome_revendedores, str_bairro, str_distancia, str_email, str_telefone, id_empresa) VALUES ( '"+nhe[0]+"', '"+nhe[1]+"', '"+nhe[2]+"', '"+nhe[3]+"', '"+nhe[4]+"', '"+str(empresa[0])+"')"
+                mycursor.execute(sql)
+                mydb.commit()
+                registros += 1 
 
-            mydb.commit()
-
-            print(mycursor.rowcount, "record inserted.")
-
+        print(registros, "registros inseridos.")
         driver.close()
-        time.sleep(10)
+        time.sleep(2)
